149f5b55fc75725d143a2f99d98fc8b29a9a1661
Manjunatha D <manjunathad13@gmail.com>
2018-03-04 07:28:28 +0530

    Integrated with Firebase database

--- src/AppController.js
+++ src/AppController.js
@@ -1,7 +1,18 @@
-function AppController($scope, $sce) {
-	$scope.text = ' ';
+function AppController($scope, $sce, $firebaseObject) {
+	// connect to realtime database provided by firebase
+	const config = {
+		apiKey: 'AIzaSyBtjX7PjxVParlqQMtdBoWcDqFd5LGpF1o',
+		authDomain: 'angular-trix-editor.firebaseapp.com',
+		databaseURL: 'https://angular-trix-editor.firebaseio.com',
+		projectId: 'angular-trix-editor',
+		storageBucket: 'angular-trix-editor.appspot.com',
+		messagingSenderId: '911255086061'
+	};
 
-	$scope.getTrustedHtml = html => $sce.trustAsHtml(html);
+	firebase.initializeApp(config);
+	const ref = firebase.database().ref();
+	$scope.data = $firebaseObject(ref.child('data'));
+	// syncObject.$bindTo($scope, 'data');
 }
 
 module.exports = AppController;
--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 require('angular');
 require('angular-trix');
 require('angular-sanitize');
+require('firebase');
+require('angularfire');
 
 const AppController = require('./AppController');
 
 const app = angular
-	.module('app', ['angularTrix', 'ngSanitize'])
+	.module('app', ['angularTrix', 'ngSanitize', 'firebase'])
 	.controller('appController', AppController);
